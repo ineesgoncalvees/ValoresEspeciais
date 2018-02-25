11f37f69619e61102d6284645111dd0ee5f1d7e0
Ines <ines98goncalves@gmail.com>
2018-02-25 15:34:06 +0000

    Codigo

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ValoresEspeciais
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ValoresEspeciais {
+    class Program {
+        static void Main(string[] args) {
+            double x = double.PositiveInfinity;
+            double y = double.NegativeInfinity;
+            double z = double.NaN;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            sbyte sb = sbyte.MaxValue;
+            double db = double.MinValue;
+
+            Console.WriteLine(sb);
+            Console.WriteLine(db);
         }
     }
 }
